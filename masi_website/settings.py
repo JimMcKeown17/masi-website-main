488713264c14439be979128331a6f1a8e03769b5
Jim McKeown <mckeown.james@gmail.com>
2025-04-07 21:11:08 -0400

    added csrf certficates

--- masi_website/settings.py
+++ masi_website/settings.py
@@ -185,3 +185,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://masinyusane.org',
+    'https://www.masinyusane.org',
+]
